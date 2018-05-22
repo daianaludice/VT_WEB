449c995092522492bce7b84c952e547c0a763e42
root <root@ip-172-31-86-160.ec2.internal>
2018-05-22 05:47:23 +0000

    front-end modify

--- show_smart_mirror_screen.php
+++ show_smart_mirror_screen.php
@@ -15,7 +15,7 @@ $query2 = "SELECT x,y,height,width,show_addr FROM Coordinate where position='upp
 $res2 =  mysqli_query($conn,$query2);
 $row2 = mysqli_fetch_array($res2);
 
-$addr = $row2['show_addr'];
+$addr =  $row2['show_addr'];
 
 ?>
 
@@ -25,11 +25,6 @@ $addr = $row2['show_addr'];
 <link rel="stylesheet" href="assets/css/show_clothe_css.css" />
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.3.min.js"></script>
 <meta http-equiv="refresh" content="1">
-<?
-$myfile = fopen("show_mirror.txt", "r") or die("Unable to open file!");
-$addr = fgets($myfile);
-fclose($myfile);
-?>
 <style>
  #img{
   padding-left : <? echo $row2['y']?>%;
@@ -41,8 +36,10 @@ fclose($myfile);
   <p style="color:red" id ="table">
 </p>
   <?
-  $time =date("Y-m-d h:m");
-  echo "<p style='font-size:150%'>$time</p>";
+  $Y =date(" Y");
+  $m_d =date("m.d");
+  $h_i =date("h:i");
+  echo "<p style='font-size:350%; font-family: Sans-Serif'><b> $Y <br> $m_d <br> $h_i</b></p>";
   ?>
 <p id="img"><img src="images/thumbs/<? echo $addr; ?>.png" height"<? echo $row2['height']?>" width="<? echo  $row2['width']?>" alt=""/></p>
 </body>
