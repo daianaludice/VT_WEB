c3e2869ee8142549c8311dcb0532f4ddbb4e41d9
daianaludice <daianaludice@naver.com>
2018-05-27 12:24:33 -0700

    layer change

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 ﻿<?php
-$servername = "localhost";
+$servername = "34.225.233.100:3306";
 $username = "root";
 $password = "1234";
 $dbname = "VT";
@@ -33,34 +33,34 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
 	<head>
 		<title>MAIN VT</title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 		<!--[if lte IE 8]><script src="Oassets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="Oassets/css/main.css" />
+      <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.3.min.js"></script>
 		<noscript><link rel="stylesheet" href="Oassets/css/noscript.css" /></noscript>
 		<!--[if lte IE 8]><link rel="stylesheet" href="Oassets/css/ie8.css" /><![endif]-->
 	</head>
 	<body>
-
 		<!-- Wrapper-->
-			<div id="wrapper">
-
+			<div id="wrapper" style="padding-top: 112px; opacity: 1;">
 				<!-- Nav -->
-					<nav id="nav">
+		  		<nav id="nav">
 						<a href="#me" class="icon fa-home active"><span>Home</span></a>
 						<a href="#log" class="icon fa-folder"><span>Log</span></a>
 						<a href="#manual" class="icon fa-envelope"><span>Manual</span></a>
 					</nav>
 
 				<!-- Main -->
-					<div id="main">
+					<div id="main" style="height: 400px;">
 
 						<!-- Me -->
 							<article id="me" class="panel">
 								<header style="margin-top:-6%">
-									<h1>VT PROJECT</h1>
+                  <h1>VT PROJECT</h1>
 									<p style="font-size:90%"><br>VT 프로젝트는 사용자가 가진 옷의 리스트들을 보여줍니다.<br>
 									   사용자가 원하는 옷을 선택하면 거울 위로 해당 옷이 나타납니다.<br>
 			               해당 옷을 입고 싶으시다면 <b><i>WEAR</i></b> 버튼을 누르고 옷장 문을 열어주세요.</p>
+
 								</header>
 								<a href="clothes_list.php" class="jumplink pic">
 									<span class="arrow icon fa-chevron-right"><span>See my log</span></span>
@@ -104,7 +104,7 @@ $sql2 = "UPDATE Coordinate SET show_addr = NULL WHERE position = 'lower'";
 
 		<!-- Scripts -->
 			<script src="Oassets/js/jquery.min.js"></script>
-			<script src="Oassets/js/skel.min.js"></script>
+<script src="Oassets/js/skel.min.js"></script>
 			<script src="Oassets/js/skel-viewport.min.js"></script>
 			<script src="Oassets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="Oassets/js/ie/respond.min.js"></script><![endif]-->
