95a8c2dc7c32d339de5089f5c685795103d279c3
daianaludice <daianaludice@naver.com>
2018-06-01 03:40:35 -0700

    screen modify

--- show_smart_mirror_screen.php
+++ show_smart_mirror_screen.php
@@ -14,7 +14,6 @@ $dbname = "VT";
 $query2 = "SELECT x,y,height,width,show_addr FROM Coordinate where position='upper'";
 $res2 =  mysqli_query($conn,$query2);
 $row2 = mysqli_fetch_array($res2);
-
 $query = "SELECT x,y,height,width,show_addr FROM Coordinate where position='lower'";
 $res =  mysqli_query($conn,$query);
 $row = mysqli_fetch_array($res);
@@ -28,25 +27,20 @@ if($addr1 == NULL){
 if($addr2 == NULL){
   $addr2 = 'none';
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
 <link rel="stylesheet" href="assets/css/show_clothe_css.css" />
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.3.min.js"></script>
- <meta http-equiv="refresh" content="0.5">
+<meta http-equiv="refresh" content="1">
+
 <style>
- #lower{
-  left : <? echo $row['x']+74 ?>px;
- top: <? echo $row['y']+630?>px;
-  z-index : 1; }
- #upper{
-    left : <? echo $row2['x']+20?>px;
-    top: <? echo $row2['y']+10?> px;
-    z-index : 4;}
+ #img{
+  padding-left : <? echo $row2['y']?>px;
+  padding-top: <? echo $row2['x']?>px;
+ }
 </style>
 </head>
 <body>
@@ -55,16 +49,10 @@ if($addr2 == NULL){
   <?
   $Y =date(" Y");
   $m_d =date("m.d");
-  $h_i =date("h:i");
-  echo "<p style='font-size:500%; font-family: Sans-Serif'><b> $Y <br> $m_d <br> $h_i</b></p>";
+  $h_i =date("H:i");
+  echo "<p style='font-size:350%; font-family: Sans-Serif'><b> $Y <br> $m_d <br> $h_i</b></p>";
   ?>
-<?php
-if($addr1 != 'black'){
-echo "<div id='upper' style='position:fixed'><img src='images/thumbs/".$addr1.".png' height=".$row2['height']."+100' width=".$row2['width']."+918' alt=''/></div>";
-echo "<div id='lower' style='position:fixed'><img src='images/thumbs/".$addr2.".png' height=".$row['height']."+30' width=".$row['width']."+981' alt=''/></div>";
-}
-if($addr1 == 'black'){
-echo "<div id='upper' style='position:fixed'><img src='images/thumbs/".$addr1.".png' height='10' width='10' alt=''/></div>";
-echo "<div id='lower' style='position:fixed'><img src='images/thumbs/".$addr2.".png' height=".$row['height']."+30' width=".$row['width']."+981' alt=''/></div>"; }
+<p id="img" style="position:fixed"><img src="images/thumbs/<? echo $addr1; ?>.png" height"<? echo $row2['height']?>" width="<? echo  $row2['width']?>" alt=""/></p>
+<p id="img" style="position:fixed"><img src="images/thumbs/<? echo $addr2; ?>.png" height"<? echo $row['height']?>" width="<? echo  $row['width']?>" alt=""/></p>
 </body>
 </html>
